b3b146889dffb4462c4e3284ddf738ba0fdb995a
Eduardo Hentges <eduardo_1359@hotmail.com>
2024-08-15 10:26:39 -0300

    Adicionando arquivos do projeto

---
+++ source/DonwloadXMLsS3/DonwloadXMLsS3/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DownloadXMLsS3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Download = new System.Windows.Forms.Button();
+            this.txtUrls = new System.Windows.Forms.TextBox();
+            this.listBoxStatus = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClearListbox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(12, 39);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(75, 23);
+            this.Download.TabIndex = 0;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtUrls
+            // 
+            this.txtUrls.Location = new System.Drawing.Point(13, 13);
+            this.txtUrls.MaxLength = 100000000;
+            this.txtUrls.Multiline = true;
+            this.txtUrls.Name = "txtUrls";
+            this.txtUrls.Size = new System.Drawing.Size(552, 20);
+            this.txtUrls.TabIndex = 1;
+            // 
+            // listBoxStatus
+            // 
+            this.listBoxStatus.FormattingEnabled = true;
+            this.listBoxStatus.Location = new System.Drawing.Point(12, 108);
+            this.listBoxStatus.Name = "listBoxStatus";
+            this.listBoxStatus.Size = new System.Drawing.Size(553, 147);
+            this.listBoxStatus.TabIndex = 2;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(134, 48);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblProgress.TabIndex = 3;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(13, 69);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(581, 13);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Limpar URl";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClearListbox
+            // 
+            this.btnClearListbox.Location = new System.Drawing.Point(13, 262);
+            this.btnClearListbox.Name = "btnClearListbox";
+            this.btnClearListbox.Size = new System.Drawing.Size(75, 23);
+            this.btnClearListbox.TabIndex = 6;
+            this.btnClearListbox.Text = "Limpar";
+            this.btnClearListbox.UseVisualStyleBackColor = true;
+            this.btnClearListbox.Click += new System.EventHandler(this.btnClearListBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClearListbox);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.listBoxStatus);
+            this.Controls.Add(this.txtUrls);
+            this.Controls.Add(this.Download);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.TextBox txtUrls;
+        private System.Windows.Forms.ListBox listBoxStatus;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClearListbox;
+    }
+}
+
---
+++ source/DonwloadXMLsS3/DonwloadXMLsS3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadXMLsS3
+{
+    public partial class Form1 : Form
+    {
+        private const int BatchSize = 50; // Tamanho do lote
+        private int totalFiles = 0;
+        private int filesDownloaded = 0;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+                string[] urls = txtUrls.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                totalFiles = urls.Length;
+                filesDownloaded = 0;
+                lblProgress.Text = $"Progress: 0/{totalFiles}";
+
+                // Initialize CancellationTokenSource
+                cancellationTokenSource = new CancellationTokenSource();
+                CancellationToken token = cancellationTokenSource.Token;
+
+                try
+                {
+                    for (int i = 0; i < urls.Length; i += BatchSize)
+                    {
+                        if (token.IsCancellationRequested)
+                        {
+                            listBoxStatus.Items.Add("Download canceled.");
+                            break;
+                        }
+
+                        var batch = urls.Skip(i).Take(BatchSize);
+                        var downloadTasks = batch.Select(url => DownloadAndSaveXML(url, folderPath, token)).ToArray();
+
+                        await Task.WhenAll(downloadTasks);
+                        filesDownloaded += downloadTasks.Length;
+                        lblProgress.Text = $"Progress: {filesDownloaded}/{totalFiles}";
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    listBoxStatus.Items.Add("Download canceled.");
+                }
+                catch (Exception ex)
+                {
+                    listBoxStatus.Items.Add($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private async Task DownloadAndSaveXML(string url, string folderPath, CancellationToken token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Set timeout or other configurations if necessary
+                    client.Timeout = TimeSpan.FromMinutes(5);
+
+                    // Use GetAsync and read the response content
+                    using (var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var xmlContent = await response.Content.ReadAsStringAsync();
+
+                        // Extrair o nome do arquivo da URL
+                        string fileName = Path.GetFileName(url);
+                        string filePath = Path.Combine(folderPath, fileName);
+
+                        // Salvar o arquivo XML
+                        File.WriteAllText(filePath, xmlContent);
+                        listBoxStatus.Items.Add($"Downloaded: {filePath}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBoxStatus.Items.Add($"Error downloading {url}: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Cancelar o download
+            cancellationTokenSource?.Cancel();
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUrls.Clear();
+           
+        }
+
+        private void btnClearListBox_Click(object sender, EventArgs e)
+        {
+            
+            listBoxStatus.Items.Clear();
+        }
+    }
+}
